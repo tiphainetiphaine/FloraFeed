93c25c876299ac30a3c7c61c112cc9299490a55e
Tiphaine <tiphainemorganemarie@gmail.com>
2023-09-20 21:28:40 +0100

    Initial Commit

---
+++ FloraFeed/FloraFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloraFeedApp.swift
+//  FloraFeed
+//
+//  Created by Tiphaine Brydniak on 20/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct FloraFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
